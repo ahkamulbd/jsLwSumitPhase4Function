98185d24c5b4b2b46d421d2aa4f2139bb837ff05
ahkamulbd <ahkamulbd@gmail.com>
2023-07-21 17:50:37 +0600

    Rest of the codes of Phase 4 added

--- js/callbackFunction.js
+++ js/callbackFunction.js
@@ -52,5 +52,24 @@ function calculator(num1, num2, callback) {
 }
 
 calculator(10, 30, function (result) {
-    console.log(result);
+    //console.log(result);
 });
+
+const myNumbers = [4, 1, -20, -7, 5, 9, -6, 7, 10, -11, 3];
+
+const posNumbers = removeNeg(myNumbers, (x) => x >= 0);
+// const posNumbers = removeNeg(myNumbers, function (x) {
+//     return x => 0;
+// });
+
+console.log(posNumbers);
+
+function removeNeg(numbers, callback) {
+    const myArray = [];
+    for (const x of numbers) {
+        if (callback(x)) {
+            myArray.push(x);
+        }
+    }
+    return myArray;
+}
